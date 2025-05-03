be1c99802b6f799066487ea7c647e961bf606720
κασσάνδρα.eth <0xDADA@protonmail.com>
2025-05-03 14:38:39 +0200

    feat: track Donation events

--- src/checker.rs
+++ src/checker.rs
@@ -4,6 +4,8 @@ use alloy::dyn_abi::DynSolValue;
 use alloy::json_abi::JsonAbi;
 use alloy::primitives::{Address, FixedBytes};
 use alloy::providers::Provider;
+use alloy::rpc::types::{BlockNumberOrTag::Latest, Filter, Log};
+use alloy::sol;
 use eyre::Result;
 use reqwest::Client;
 use serde::Deserialize;
@@ -31,32 +33,103 @@ pub struct Checker<P: Provider> {
     http_client: Client,
     pg_client: DbClient,
     provider: P,
+    start_block: u64,
+    filter: Filter,
+}
+
+sol! {
+    interface IDonate {
+        event Donation(address indexed donor, uint256 amount);
+    }
 }
 
 impl<P: Provider> Checker<P> {
     pub fn new(
-        target_address: String,
+        target_transfer_address: String,
+        target_donation_address: String,
         etherscan_api_key: String,
         provider: P,
         http_client: Client,
         pg_client: DbClient,
+        start_block: u64,
     ) -> Self {
         Self {
-            target_address,
+            target_address: target_transfer_address.clone(),
             etherscan_api_key,
             provider,
             http_client,
             pg_client,
+            start_block,
+            filter: Filter::new()
+                .address(vec![
+                    Address::from_str(&target_donation_address.clone()).unwrap()
+                ]),
         }
     }
 
     pub async fn run(&self) {
+        if let Err(err) = self.process_past_logs().await {
+            eprintln!("Error processing past logs: {}", err);
+        }
+
         loop {
             if let Err(e) = self.check_transfers().await {
                 println!("Error checking transfers: {}", e);
             }
-            tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+            if let Err(e) = self.process_new_logs().await {
+                println!("Error checking donations: {}", e);
+            }
+            // TODO: add live donations log processing here
+            tokio::time::sleep(tokio::time::Duration::from_secs(30)).await;
         }
+    }
+
+    pub async fn process_past_logs(&self) -> Result<()> {
+        let block = self.provider.get_block_by_number(Latest).await?;
+        let mut current_end_block = block.unwrap().header.number;
+
+        while self.start_block < current_end_block {
+            let current_start_block = if current_end_block >= 49999 {
+                current_end_block - 49999
+            } else {
+                0
+            };
+
+            let filter = self
+                .filter
+                .clone()
+                .from_block(current_start_block)
+                .to_block(current_end_block);
+
+            // Fetch logs
+            let logs = self.provider.get_logs(&filter).await?;
+            for log in logs {
+                self.process_donation_event(log.clone()).await?;
+            }
+
+            current_end_block = current_start_block;
+        }
+        Ok(())
+    }
+
+    pub async fn process_new_logs(&self) -> Result<()> {
+        let block = self.provider.get_block_by_number(Latest).await?;
+        let current_end_block = block.unwrap().header.number;
+        let current_start_block = if current_end_block >= 64 {
+            current_end_block - 64
+        } else {
+            0
+        };
+        let filter = self
+            .filter
+            .clone()
+            .from_block(current_start_block)
+            .to_block(current_end_block);
+        let logs = self.provider.get_logs(&filter).await?;
+        for log in logs {
+            self.process_donation_event(log.clone()).await?;
+        }
+        Ok(())
     }
 
     pub async fn check_transfers(&self) -> Result<()> {
@@ -106,7 +179,7 @@ impl<P: Provider> Checker<P> {
                 let value_in_wei: u128 = tx.value.parse().unwrap_or(0);
                 if value_in_wei > 0 {
                     // skip failed txs
-                    let hash_key = generate_hash_key(&tx.value, &tx.from, &tx.hash);
+                    let hash_key = generate_transfer_hash_key(&tx.value, &tx.from, &tx.hash);
 
                     // Query if this hash_key already exists
                     let exists = self
@@ -127,7 +200,10 @@ impl<P: Provider> Checker<P> {
                     let value_in_wei: u128 = tx.value.parse().unwrap_or(0);
                     let eth_amount = format!("{:.18}", (value_in_wei as f64) / 1e18f64);
 
-                    println!("From: {} Amount: {} ETH", from_display, eth_amount);
+                    println!(
+                        "TRANSFER -- From: {} Amount: {} ETH",
+                        from_display, eth_amount
+                    );
 
                     // Insert new entry into Postgres
                     self.pg_client
@@ -137,6 +213,55 @@ impl<P: Provider> Checker<P> {
             }
         }
 
+        Ok(())
+    }
+
+    pub async fn process_donation_event(&self, log: Log) -> Result<()> {
+        match log.log_decode::<IDonate::Donation>() {
+            Ok(decoded_log) => {
+                let tx_hash = log.transaction_hash.unwrap_or_default().to_string();
+                let log_index = log.log_index.unwrap_or_default().to_string();
+                let amount = decoded_log.inner.amount.to_string();
+                let donor = decoded_log.inner.donor.to_string();
+                let hash_key = generate_donation_hash_key(&amount, &donor, &tx_hash, &log_index);
+
+                // Query if this hash_key already exists
+                let exists = self
+                    .pg_client
+                    .check_donation_exists(hash_key.clone())
+                    .await?;
+                let mut from_display = "".to_string();
+                if !exists {
+                    // If it's a new transaction: resolve ENS
+                    from_display =
+                        match resolve_ens_name(&self.provider, decoded_log.inner.donor).await {
+                            Some(name) => name,
+                            None => donor.clone(),
+                        };
+
+                    println!("DONATION -- From: {}", from_display);
+                }
+
+                let value_in_wei: u128 = amount.parse().unwrap_or(0);
+                let eth_amount = format!("{:.18}", (value_in_wei as f64) / 1e18f64);
+
+                // Insert new entry into Postgres
+                self.pg_client
+                    .insert_donation(
+                        decoded_log.removed,
+                        tx_hash,
+                        log_index,
+                        donor,
+                        eth_amount,
+                        hash_key,
+                        from_display,
+                    )
+                    .await?;
+            }
+            Err(e) => {
+                println!("Failed to decode Donation event: {:?}", e);
+            }
+        }
         Ok(())
     }
 }
@@ -211,8 +336,21 @@ pub fn namehash(name: &str) -> Option<String> {
     Some("0x".to_string() + &hex::encode(node))
 }
 
-pub fn generate_hash_key(amount_wei: &str, from_address: &str, tx_hash: &str) -> String {
+pub fn generate_transfer_hash_key(amount_wei: &str, from_address: &str, tx_hash: &str) -> String {
     let input = format!("{}{}{}", amount_wei, from_address, tx_hash);
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    hex::encode(result)
+}
+
+pub fn generate_donation_hash_key(
+    amount_wei: &str,
+    from_address: &str,
+    tx_hash: &str,
+    log_index: &str,
+) -> String {
+    let input = format!("{}{}{}{}", amount_wei, from_address, tx_hash, log_index);
     let mut hasher = Sha256::new();
     hasher.update(input);
     let result = hasher.finalize();
